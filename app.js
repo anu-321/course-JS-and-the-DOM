2c598c765562fe5f2bad2cb121cf4d07c5d17e0f
anu-321 <myeducanisa@gmail.com>
2025-02-04 00:52:13 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const header = document.querySelector("h1");
+const originalText = header.textContent;
+const updatedText = "Build an AR app";
+header.addEventListener("mouseover", function () {
+    header.textContent = updatedText;
+  });
+  header.addEventListener("mouseout", function () {
+    header.textContent = originalText;
+  });
