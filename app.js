575de69a82b02925cced9a071b40e742afd321d6
anu-321 <myeducanisa@gmail.com>
2025-02-05 03:16:35 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
 const ndList = document.querySelectorAll('.card--nanodegree__title');
+ndList.forEach(function(nd){const button=document.createElement("button");button.textContent="Add to favorites";button.classList='button button--primary';nd.appendChild(button);});
